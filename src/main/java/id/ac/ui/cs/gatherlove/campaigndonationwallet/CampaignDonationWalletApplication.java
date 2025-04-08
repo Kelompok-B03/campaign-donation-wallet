ec1c2e3a16f84a2d65a7b1523ceecb0b1b8d4a43
abyasa05 <abyprat07@gmail.com>
2025-04-08 10:22:09 +0700

    Initialize campaign-donation-wallet

---
+++ src/main/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/CampaignDonationWalletApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.gatherlove.campaigndonationwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignDonationWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampaignDonationWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/CampaignDonationWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.gatherlove.campaigndonationwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignDonationWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
