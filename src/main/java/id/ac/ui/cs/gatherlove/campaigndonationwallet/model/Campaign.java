7ce21cd512e942bd6e67558e28724280c4df4a68
Paima <paimapurba@gmail.com>
2025-04-11 19:20:32 +0700

    [GREEN] Add Campaign Model

---
+++ src/main/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/model/Campaign.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.gatherlove.campaigndonationwallet.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "campaigns")
+public class Campaign {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) // atau IDENTITY jika kamu pakai Long
+    private String campaignId;
+
+    private String title;
+    private String description;
+    private int targetAmount;
+    private int fundsCollected;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String fundraiserId;
+    private String status;
+}
