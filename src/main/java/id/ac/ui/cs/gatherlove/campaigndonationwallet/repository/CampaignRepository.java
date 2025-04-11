349dc83939aa0feb265f4f09fdbf4823a5c62fac
Paima <paimapurba@gmail.com>
2025-04-11 19:23:37 +0700

    [GREEN] Implement CampaignRepository

---
+++ src/main/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/repository/CampaignRepository.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.gatherlove.campaigndonationwallet.repository;
+
+import id.ac.ui.cs.gatherlove.campaigndonationwallet.model.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CampaignRepository extends JpaRepository<Campaign, String> {
+    List<Campaign> findByFundraiserId(String fundraiserId);
+}
