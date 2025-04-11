005edf815e01e2b113a2a531bd9d4a8f7d344315
abyasa05 <abyprat07@gmail.com>
2025-04-11 20:51:01 +0700

    [RED] add skeleton for donation repository

---
+++ src/main/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/repository/DonationRepository.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.gatherlove.campaigndonationwallet.repository;
+
+import id.ac.ui.cs.gatherlove.campaigndonationwallet.model.Donation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface DonationRepository extends JpaRepository<Donation, UUID> {
+
+    // Skeleton methods (to be implemented/customized later)
+
+    List<Donation> findByUserId(UUID userId);
+
+    List<Donation> findByCampaignId(UUID campaignId);
+
+    List<Donation> findByStateName(String stateName);
+}
+
