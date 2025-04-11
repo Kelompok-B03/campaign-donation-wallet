04d75aacbb9dd60203cfc21d4f640f7efd5860f8
FilbertAT <filbertaurelian@gmail.com>
2025-04-11 20:54:54 +0700

    [GREEN] configure dotenv for test environment setup

--- src/test/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/CampaignDonationWalletApplicationTests.java
+++ src/test/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/CampaignDonationWalletApplicationTests.java
@@ -1,10 +1,20 @@
 package id.ac.ui.cs.gatherlove.campaigndonationwallet;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class CampaignDonationWalletApplicationTests {
+
+    static {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
 
     @Test
     void contextLoads() {
