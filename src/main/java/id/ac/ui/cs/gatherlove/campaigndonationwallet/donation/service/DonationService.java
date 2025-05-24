4bbeba85927eb39781fea3a235fdd59e3a0d07e1
abyasa05 <abyprat07@gmail.com>
2025-05-24 20:10:48 +0700

    [REFACTOR] Incr. campaign funds in createDonation & add updateStatusByCampaign method

--- src/main/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/donation/service/DonationService.java
+++ src/main/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/donation/service/DonationService.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface DonationService {
     Donation createDonation(String campaignId, Float amount, String message);
     Donation updateStatus(UUID donationId);
+    List<Donation> updateStatusByCampaign(String campaignId);
     void deleteDonation(UUID donationId);
     Donation getDonationById(UUID donationId);
     List<Donation> getDonationsByUserId(UUID userId);
