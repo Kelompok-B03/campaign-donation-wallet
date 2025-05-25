9692c8fd29738c1492f753a4acbb224e87cb5d16
abyasa05 <abyprat07@gmail.com>
2025-05-25 10:17:04 +0700

    [REFACTOR] Add donation count method for admin

--- src/main/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/donation/service/DonationService.java
+++ src/main/java/id/ac/ui/cs/gatherlove/campaigndonationwallet/donation/service/DonationService.java
@@ -14,4 +14,5 @@ public interface DonationService {
     List<Donation> getDonationsByUserId(UUID userId);
     List<Donation> getDonationsByCampaignId(String campaignId);
     List<Donation> getSelfDonations();
+    long getDonationsCount();
 }
